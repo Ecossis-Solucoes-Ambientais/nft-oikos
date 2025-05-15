b75980cd2f0f478ac537c1dd7386d9abba646031
Thiago Rossi <tj.angrizanes@gmail.com>
2025-05-15 15:45:07 -0300

    Update Header.js

--- oikos-certify-ui/src/components/Header.js
+++ oikos-certify-ui/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="bg-white shadow p-4 flex justify-between max-w-5xl mx-auto">
-      <h1 className="font-bold text-xl">Certify</h1>
+      <h1 className="font-bold text-xl">Oikos Certify</h1>
       <nav className="space-x-4">
         <Link to="/" className="hover:underline">Início</Link>
         <Link to="/gallery" className="hover:underline">Galeria</Link>
