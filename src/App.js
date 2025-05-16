265963afdb66987b367835eaeff3388c97b82327
Thiago Rossi <tj.angrizanes@gmail.com>
2025-05-15 22:19:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Upload  from './pages/Upload';
-import Gallery from './pages/Gallery';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import Header    from './components/Header'
+import Upload    from './pages/Upload'
+import Gallery   from './pages/Gallery'
 
-function App() {
+export default function App() {
   return (
-    <Router basename="/nft-oikos">
-      <Header />
+    <Router>
+      <Header/>
       <Routes>
-        <Route path="/" element={<Upload />} />
+        <Route path="/"       element={<Upload />} />
         <Route path="/gallery" element={<Gallery />} />
       </Routes>
     </Router>
-  );
+  )
 }
-
-export default App;
