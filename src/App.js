d1cdd7d450ecf4f97e6600c8cc52e96c3741d08d
Thiago Rossi <tj.angrizanes@gmail.com>
2025-05-17 14:58:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header  from './components/Header'
 import Upload  from './pages/Upload'
 import Gallery from './pages/Gallery'
+import CertificatesDetail from './pages/CertificatesDetail'
 
 export default function App() {
   return (
