104c37813aa3958dc601b76f36021f26e8d634a6
Thiago Rossi <tj.angrizanes@gmail.com>
2025-05-17 14:56:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Upload />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/certificates/:tokenId" element={<CertificateDetail />} />
       </Routes>
     </Router>
   )
